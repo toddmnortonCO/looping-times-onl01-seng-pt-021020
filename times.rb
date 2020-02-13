18b04e7a28b7b0269b5084fe2f35f15ea7f7c569
Todd Norton <toddmnorton@live.com>
2020-02-13 20:29:23 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -1,7 +1,10 @@
+what_number = 50
+
 def using_times
-  3.times do
-    puts 
-	#your code here
+  4.times do
+    puts "What number are we at?"
+    what_number = what_number - 5
+    puts "Well we're at #{what_number} now."
 end
 
 
